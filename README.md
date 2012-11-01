42b7239d76647acf7dcbbee68b9929872822ac31
Baptiste Langlade <baptouuuu@gmail.com>
2012-11-01 18:57:56 +0100

    first commit
    indexeddb.js and test file added

--- README.md
+++ README.md
@@ -1,4 +1,181 @@
 IndexedDB.js
 ============
 
-Wrapper to manipulare easily IndexedDB
+IndexedDB.js let's you manipulate easily this database in your application. Before using it, make sure it's working properly on your targeted browsers.
+
+It has been tested with Chrome 21+ and Firefox 15+.
+
+You can open the test.html file to see it works on your browsers.
+
+Usage
+-----
+The constructor can take up to 5 parameters:
+```javascript
+	database: database name
+	version: version number (integer only)
+	structure: object representing your database (example below)
+	openingCallback: function called when the database is opened
+	upgradeCallback: function called when the database is upgraded via the old way using setVersion() (like in chrome)
+	
+	opening and upgrade callbacks parameters are objects built like this:
+	{
+		fn: 'function to call',
+		context: 'object'
+	}
+```
+Structure object example:
+```javascript
+{
+	storeName: {
+		options: {
+			keyPath: 'object property used as keyPath',
+			autoIncrement: true|false
+		},
+		indexes: [	//can be an array or a single object
+			{
+				name: 'index name',
+				keyPath: 'object property used for the index',
+				options: {unique: true|false}
+			}
+		],
+		defaultValues: [	//can be an array of objects or a single object
+			{name: 'object inserted at the database creation only'}
+		]
+	}
+}
+```
+
+The object provide a basic CRUD and some other functions such as readAll, empty, find and destroyDatabase.
+
+### `IndexedDB.create()`
+```javascript
+	IndexedDB.create(
+		'objectStoreName', 
+		{object: 'to store'},	//do not specify the keyPath property
+		function (event) {
+			//will be called when the object is stored
+			
+			var keyPath = event.target.result;
+		},
+		function (event) {
+			//will be called if indexeddb fails to create the object
+		},
+		contextObjectAppliedOnCallbacks
+	);
+```
+
+### `IndexedDB.read()`
+```javascript
+	IndexedDB.read(
+		'objectStoreName',
+		'keyPathValue',
+		function (event) {
+			//will be called when the object is retrieved
+			
+			var wishedObject = event.target.result;
+		},
+		function (event) {
+			//will be called if indexeddb fails to retrieve the object
+		},
+		contextObjectAppliedOnCallbacks
+	);
+```
+
+### `IndexedDB.readAll()`
+```javascript
+	IndexedDB.readAll(
+		'objectStoreName',
+		function (event) {
+			//will be called when objects are retrieved
+			//all objects are not retrieved at once
+			
+			var result = event.target.result;
+			
+			if (!!result === false) {
+			
+				//no more object to retrieve
+				return;
+			
+			}
+			
+			myObject = result.value;
+			
+			//continue to the next object (this will recall this function)
+			result.continue();
+		},
+		function (event) {
+			//will be called if indexeddb fails to retrieve objects
+		},
+		contextObjectAppliedOnCallbacks
+	);
+```
+
+### `IndexedDB.update()`
+
+Alias of the create method, but this time in the object parameter the keyPath property has to be specified (in order to match the object in the database)
+
+### `IndexedDB.remove()`
+```javascript
+	IndexedDB.remove(
+		'objectStoreName',
+		'keyPathValue',
+		function (event) {
+			//will be called if the object is deleted
+		},
+		function (event) {
+			//will be called if indexeddb fails to delete the object
+		},
+		contextObjectAppliedOnCallbacks
+	);
+```
+
+### `IndexedDB.empty()`
+```javascript
+	IndexedDB.empty(
+		'objectStoreName',
+		function (event) {
+			//will be called when the objectStore has been emptied
+		},
+		function (event) {
+			//will be called if indexeddb fails to empty the objectStore
+		},
+		contextObjectAppliedOnCallbacks
+	);
+```
+
+### `IndexedDB.find()`
+```javascript
+	IndexedDB.find(
+		'objectStoreName',
+		'indexName',
+		'value to search'|[lowerBound, upperBound], //if array bounds are included in the search
+		function (event) {
+			//will be called when objects are retrieved
+			//all objects are not retrieved at once
+			
+			var result = event.target.result;
+			
+			if (!!result === false) {
+			
+				//no more object to retrieve
+				return;
+			
+			}
+			
+			myObject = result.value;
+			
+			//continue to the next object (this will recall this function)
+			result.continue();
+		},
+		function (event) {
+			//will be called if indexeddb fails to retrieve objects
+		},
+		contextObjectAppliedOnCallbacks
+	);
+```
+
+### `IndexedDB.close()`
+Close the connection to the database.
+
+### `IndexedDB.destroyDatabase()`
+Delete the whole database, no survivors.
---
+++ indexeddb.js
@@ -0,0 +1,558 @@
+/**
+	* This is a wrapper to IndexedDB databases
+	*
+	* Example object for the structure:
+	* <code>
+	*	{
+	*		storeName: {
+	*			options: {
+	*				keyPath: 'object property used as keyPath',
+	*				autoIncrement: true|false
+	*			},
+	*			indexes: [
+	*				{
+	*					name: 'index name',
+	*					keyPath: 'object property used for the index',
+	*					options: {unique: true|false}
+	*				}
+	*			],
+	*			defaultValues: [
+	*				{name: 'default object to insert by default'}
+	*			]
+	*		}
+	*	}
+	* </code>
+	* Default values will be inserted only when the database is created with version number to 1
+	*
+	* @param	string		[database] Database name
+	* @param	integer		[version] Database version number
+	* @param	object		[structure] See description above
+	* @param	function	[openingCallback] {fn: 'function to call', context: 'object'}
+	* @param	object		[upgradeCallback] {fn: 'function to call', context: 'object'}
+*/
+
+var IndexedDB = function (database, version, structure, openingCallback, upgradeCallback) {
+
+	this.connection = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+	this.transaction = window.IDBTransaction || window.webkitIDBTransaction;
+	this.keyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
+	this.transactionModes = {
+		READ_ONLY: 'readonly',
+		READ_WRITE: 'readwrite'
+	};
+	
+	this.db = null;
+	
+	this.dbName = database || 'default';
+	this.version = version || 1;
+	this.structure = structure || {
+		defaultStore: {
+			options: {
+				keyPath: 'id', 
+				autoIncrement: true
+			},
+			indexes: {
+				name: 'id', 
+				keyPath: 'id', 
+				options: {unique: false}
+			},
+			defaultValues: {}
+		}
+	};
+	this.openingCallback = openingCallback;
+	this.upgradeCallback = upgradeCallback;
+	
+	this.isReady = false;
+	
+	this.open();
+
+};
+
+IndexedDB.prototype = {
+
+	open: function () {
+	
+		var self = this,
+			request = self.connection.open(self.dbName, self.version);
+		
+		console.log('Opening the database "' + self.dbName + '"...');
+		
+		request.onupgradeneeded = function (event) {
+			self.dbUpgrade.call(self, event, false);
+		};
+		
+		request.onsuccess = function (event) {
+			self.requestSuccess.call(self, event);
+		};
+		
+		request.onerror = function (event) {
+			console.log('***DATABASE ERROR***', event);
+		};
+		
+		request.onblocked = function (event) {
+			console.log('***DATABASE BLOCKED***', event);
+		};
+	
+	},
+	
+	/**
+		* Called when the db opening succeeded
+	*/
+	
+	requestSuccess: function (event) {
+	
+		var self = this;
+		
+		self.db = event.target.result;
+		
+		self.db.onerror = function (event) {
+			console.log('***DATABASE ERROR***', event);
+		};
+		
+		//old way to update db until Google Chrome move to onupgradeneeded
+		if (self.version !== self.db.version && typeof self.db.setVersion === 'function') {
+		
+			var versionRequest = self.db.setVersion(self.version);
+			
+			versionRequest.onsuccess = function (event) {
+				self.dbUpgrade.call(self, event, true);
+			};
+		
+		}
+		
+		self.isReady = true;
+		
+		console.log('Database opened!');
+		
+		/* execute callback here only if no database upgrade needed */
+		if (self.openingCallback !== undefined && self.version === self.db.version) {
+		
+			self.openingCallback.fn.call(self.openingCallback.context);
+		
+		}
+	
+	},
+	
+	/**
+		* Called if the database version number has been incremented
+	*/
+	
+	dbUpgrade: function (event, chrome) {
+	
+		var self = this;
+		
+		self.isReady = false;
+		
+		console.log('Upgrading database...');
+		
+		if (chrome === true) {
+		
+			self.db = event.target.source;
+		
+		} else {
+		
+			self.db = event.target.result;
+		
+		}
+		
+		for (var storeName in self.structure) {
+		
+			if (!self.db.objectStoreNames.contains(storeName)) {
+			
+				var objectStore = self.db.createObjectStore(
+					storeName,
+					self.structure[storeName].options
+				);
+				
+				if (self.structure[storeName].defaultValues !== undefined && self.version === 1) {
+				
+					var values = self.structure[storeName].defaultValues;
+					
+					if (values instanceof Array) {
+						
+						for (var i = 0, l = values.length; i < l; i++) {
+						
+							objectStore.put(values[i]);
+						
+						}
+					
+					} else if (values instanceof Object) {
+					
+						objectStore.put(values);
+					
+					}
+				
+				}
+			
+			}
+		
+			/* create indexes if there're not existing */
+			if (self.structure[storeName].indexes !== undefined) {
+			
+				var indexes = self.structure[storeName].indexes,
+					objectStore = event.target.transaction.objectStore(storeName);
+				
+				if (indexes instanceof Array) {
+				
+					for (var i = 0, l = indexes.length; i < l; i++) {
+					
+						if (!objectStore.indexNames.contains(indexes[i].name)) {
+						
+							objectStore.createIndex(indexes[i].name, indexes[i].keyPath, indexes[i].options);
+						
+						}
+					
+					}
+				
+				} else if (indexes instanceof Object && !objectStore.indexNames.contains(indexes.name)) {
+				
+					objectStore.createIndex(indexes.name, indexes.keyPath, indexes.options);
+				
+				}
+			
+			}
+		
+		}
+		
+		//deletion of stores non present in the database structure
+		for (var i = 0, l = self.db.objectStoreNames.length; i < l; i++) {
+		
+			if (self.structure[self.db.objectStoreNames[i]] === undefined) {
+			
+				self.db.deleteObjectStore(self.db.objectStoreNames[i]);
+			
+			}
+		
+		}
+		
+		console.log('Database upgraded to version: ' + self.version + '!');
+		
+		self.isReady = true;
+		
+		/* same function as openingCallback but only for chrome; because chrome open the database before upgrading, the normal flow is upgrade then open the database */
+		if (self.upgradeCallback !== undefined && chrome === true) {
+		
+			self.upgradeCallback.fn.call(self.openingCallback.context, event);
+		
+		}
+	
+	},
+	
+	/**
+		* Create an object into the store
+		* keyPath returned via event.target.result
+	*/
+	
+	create: function (storeName, object, onSuccess, onError, context) {
+	
+		if (this.isReady === false || this.structure[storeName] === undefined) {
+		
+			return;
+		
+		}
+		
+		try {
+		
+			var transaction = this.db.transaction([storeName], this.transactionModes.READ_WRITE),
+				store = transaction.objectStore(storeName),
+				request = store.put(object),
+				context = context || window;
+			
+			if (onSuccess !== undefined) {
+			
+				request.onsuccess = function (event) {
+					onSuccess.call(context, event);
+				};
+			
+			}
+			
+			if (onError !== undefined) {
+			
+				request.onerror = function (event) {
+					onError.call(context, event);
+				};
+			
+			}
+		
+		} catch (error) {
+		
+			if (onError !== undefined) {
+			
+				onError.call(context, error);
+			
+			}
+		
+		}
+	
+	},
+	
+	/**
+		* Read an object from the store
+		* object returned via event.target.result
+	*/
+	
+	read: function (storeName, id, onSuccess, onError, context) {
+	
+		if (this.isReady === false || this.structure[storeName] === undefined) {
+		
+			return;
+		
+		}
+		
+		try {
+		
+			var transaction = this.db.transaction([storeName], this.transactionModes.READ_ONLY),
+				store = transaction.objectStore(storeName),
+				request = store.get(id),
+				context = context || window;
+				
+			if (onSuccess !== undefined) {
+			
+				request.onsuccess = function(event) {
+					onSuccess.call(context, event);
+				};
+			
+			}
+			
+			if (onError !== undefined) {
+			
+				request.onerror = function(event) {
+					onError.call(context, event);
+				};
+			
+			}
+		
+		} catch (error) {
+		
+			if (onError !== undefined) {
+			
+				onError.call(context, error);
+			
+			}
+		
+		}
+	
+	},
+	
+	/**
+		* Read all items of an object store
+	*/
+	
+	readAll: function (storeName, onSuccess, onError, context) {
+	
+		if (this.isReady === false || this.structure[storeName] === undefined) {
+		
+			return;
+		
+		}
+		
+		try {
+		
+			var transaction = this.db.transaction([storeName], this.transactionModes.READ_ONLY),
+				store = transaction.objectStore(storeName),
+				keyRange = this.keyRange.lowerBound(0),
+				request = store.openCursor(keyRange),
+				context = context || window;
+			
+			if (onSuccess !== undefined) {
+			
+				request.onsuccess = function(event) {
+					onSuccess.call(context, event);
+				};
+			
+			}
+			
+			if (onError !== undefined) {
+			
+				request.onerror = function(event) {
+					onError.call(context, event);
+				};
+			
+			}
+		
+		} catch (error) {
+		
+			if (onError !== undefined) {
+			
+				onError.call(context, error);
+			
+			}
+		
+		}
+	
+	},
+	
+	/**
+		* Update an object inside a store
+	*/
+	
+	update: function (storeName, object, onSuccess, onError, context) {
+	
+		this.create(storeName, object, onSucces, onError, context);
+	
+	},
+	
+	/**
+		* Delete an object inside a store
+	*/
+	
+	remove: function (storeName, id, onSuccess, onError, context) {
+	
+		if (this.isReady === false || this.structure[storeName] === undefined) {
+		
+			return;
+		
+		}
+		
+		try {
+		
+			var transaction = this.db.transaction([storeName], this.transactionModes.READ_WRITE),
+				store = transaction.objectStore(storeName),
+				request = store.delete(id),
+				context = context || window;
+			
+			if (onSuccess !== undefined) {
+			
+				request.onsuccess = function (event) {
+					onSuccess.call(context, event);
+				};
+			
+			}
+			
+			if (onError !== undefined) {
+			
+				request.onerror = function (event) {
+					onError.call(context, event);
+				};
+			
+			}
+		
+		} catch (error) {
+		
+			if (onError !== undefined) {
+			
+				onError.call(context, error);
+			
+			}
+		
+		}
+	
+	},
+	
+	/**
+		* Delete all objects inside an object store
+	*/
+	
+	empty: function (storeName, onSuccess, onError, context) {
+	
+		if (this.isReady === false || this.structure[storeName] === undefined) {
+		
+			return;
+		
+		}
+		
+		try {
+		
+			var transaction = this.db.transaction([storeName], this.transactionModes.READ_WRITE),
+				store = transaction.objectStore(storeName),
+				request = store.clear(),
+				context = context || window;
+			
+			if (onSuccess !== undefined) {
+			
+				request.onsuccess = function (event) {
+					onSuccess.call(context, event);
+				};
+			
+			}
+			
+			if (onError !== undefined) {
+			
+				request.onerror = function (event) {
+					onError.call(context, event);
+				};
+			
+			}
+		
+		} catch (error) {
+		
+			if (onError !== undefined) {
+			
+				onError.call(context, error);
+			
+			}
+		
+		}
+	
+	},
+	
+	/**
+		* Search elements with a specific term, or an array of two bounds, on an index
+	*/
+	
+	find: function (storeName, indexName, term, onSuccess, onError, context) {
+	
+		if (this.isReady === false || this.structure[storeName] === undefined) {
+		
+			return;
+		
+		}
+		
+		try {
+		
+			var transaction = this.db.transaction([storeName], this.transactionModes.READ_ONLY),
+				store = transaction.objectStore(storeName),
+				keyRange = (term instanceof Array && term.length === 2) ? this.keyRange.bound(term[0], term[1]) : this.keyRange.only(term),
+				index = store.index(indexName),
+				request = index.openCursor(keyRange),
+				context = context || window;
+			
+			if (onSuccess !== undefined) {
+			
+				request.onsuccess = function (event) {
+					onSuccess.call(context, event);
+				};
+			
+			}
+			
+			if (onError !== undefined) {
+			
+				request.onerror = function (event) {
+					onError.call(context, event);
+				};
+			
+			}
+		
+		} catch (error) {
+		
+			if (onError !== undefined) {
+			
+				onError.call(context, error);
+			
+			}
+		
+		}
+	
+	},
+	
+	/**
+		* Close database connection
+	*/
+	
+	close: function() {
+	
+		this.db.close();
+	
+	},
+	
+	/**
+		* Destroy whole database
+	*/
+	
+	destroyDatabase: function() {
+	
+		this.connection.deleteDatabase(this.dbName);
+	
+	}
+
+};
